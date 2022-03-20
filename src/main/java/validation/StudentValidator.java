5adf77f841323afffd4047e44170028b37917571
Claudiu Pana <pana_klau@yahoo.com>
2022-03-20 16:24:36 +0200

    hotfix put null check before using methods on object

--- src/main/java/validation/StudentValidator.java
+++ src/main/java/validation/StudentValidator.java
@@ -11,23 +11,23 @@ public class StudentValidator implements Validator<Student> {
      */
     @Override
     public void validate(Student entity) throws ValidationException {
-        if(entity.getID().equals("")){
-            throw new ValidationException("Id incorect!");
-        }
         if(entity.getID() == null){
             throw new ValidationException("Id incorect!");
-        }
-        if(entity.getNume() == ""){
-            throw new ValidationException("Nume incorect!");
-        }
-        if(entity.getGrupa() < 0) {
-            throw new ValidationException("Grupa incorecta!");
         }
         if(entity.getEmail() == null){
             throw new ValidationException("Email incorect!");
         }
         if(entity.getNume() == null){
             throw new ValidationException("Nume incorect!");
+        }
+        if(entity.getID().equals("")){
+            throw new ValidationException("Id incorect!");
+        }
+        if(entity.getNume().equals("")){
+            throw new ValidationException("Nume incorect!");
+        }
+        if(entity.getGrupa() < 0) {
+            throw new ValidationException("Grupa incorecta!");
         }
         if(entity.getEmail().equals("")){
             throw new ValidationException("Email incorect!");
